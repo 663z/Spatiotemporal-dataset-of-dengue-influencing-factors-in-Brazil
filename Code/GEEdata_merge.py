6f082a41b080662060906b59a7465457e14ebbe5
Qixu Zhu <1370926922@qq.com>
2025-03-25 19:48:26 +0800

    Add files via upload

---
+++ Code/GEEdata_merge.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+
+
+# Replace with your file directory path
+file_directory = 'data/PDSI_2001-2024'   
+outputpath = 'Brazil data and code/Data'
+output_file_path = os.path.join(outputpath, 'Palmer_Drought_Severity_Index.csv')
+################################################################
+
+# Get all files in the directory
+files = [f for f in os.listdir(file_directory) if f.endswith('.csv')]
+
+# Initialize an empty DataFrame to store the results of the merge
+merged_data = pd.DataFrame()
+
+# Iterate through each document
+for file in files:
+    # Read CSV files
+    file_path = os.path.join(file_directory, file)
+    data = pd.read_csv(file_path, header=0)
+    
+    # Extract the 5 digits of the filename
+    file_id = ''.join(filter(str.isdigit, file))
+    
+    # Rename columns
+    data = data.rename(columns={'mean': file_id})
+    # View column names
+    print(data.columns)  
+    
+    # Setting the date as an index
+    data = data.set_index('date')
+    
+    # Merge data
+    if merged_data.empty:
+        merged_data = data
+    else:
+        merged_data = merged_data.join(data, how='outer')
+
+# Transpose the DataFrame so that dates become column labels and file IDs become row labels
+merged_data = merged_data.T
+
+# Save the merged data to a new CSV file
+merged_data.to_csv(output_file_path)
+
+print(f'Merged data saved to {output_file_path}')
